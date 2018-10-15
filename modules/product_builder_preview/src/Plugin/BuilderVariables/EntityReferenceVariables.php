9e5d6170d22a255c9d7f41c5c6b55b4053904f92
Alexbrut <alexey.pinchuk@zanzarra.com>
2018-10-15 18:12:51 +0300

    Added recursion validation for entity reference variable.

--- modules/product_builder_preview/src/Plugin/BuilderVariables/EntityReferenceVariables.php
+++ modules/product_builder_preview/src/Plugin/BuilderVariables/EntityReferenceVariables.php
@@ -15,9 +15,26 @@ use Drupal\product_builder_preview\Plugin\BuilderVariablesPluginBase;
 class EntityReferenceVariables extends BuilderVariablesPluginBase {
 
   public function prepare($value) {
+    $entity_list = &drupal_static('pb_entity_reference_variables', []);
+    $tree = &drupal_static('pb_entity_reference_variables_tree', []);
 
     $entity_type = $this->field_definition->getSetting('target_type');
     $handler_settings = $this->field_definition->getSetting('handler_settings');
+
+    $id = $value ? $value : 'new';
+    $tree_id = $entity_type . '_' . $id;
+
+    //if there is an entity in entity tree position
+    if (array_search($tree_id, $tree) !== FALSE) {
+      return;
+    }
+
+    //If there is prepared variables for for this entity
+    if (isset($entity_list[$entity_type][$id])) {
+      return $entity_list[$entity_type][$id];
+    }
+
+    $tree[] = $tree_id;
 
     $multiple_bundles = FALSE;
     if (!empty($handler_settings['target_bundles'])) {
@@ -46,17 +63,24 @@ class EntityReferenceVariables extends BuilderVariablesPluginBase {
 
     }
 
-    if (!$value) {
-      return $prepared;
+    if ($value) {
+      $value = parent::prepare($value);
+      $referenced_entity = \Drupal::entityTypeManager()->getStorage($entity_type)->load($value);
+      if (method_exists($referenced_entity, 'getFields')) {
+        $prepared += product_builder_preview_entity_get_prepared_fields($referenced_entity);
+      }
+      else {
+        $prepared = $value;
+      }
+    }
+    else {
+      $prepared = $value;
     }
 
-    $value = parent::prepare($value);
-    $referenced_entity = \Drupal::entityTypeManager()->getStorage($entity_type)->load($value);
-    if (method_exists($referenced_entity, 'getFields')) {
-      return $prepared + product_builder_preview_entity_get_prepared_fields($referenced_entity);
-    }
+    $entity_list[$entity_type][$id] = $prepared;
+    array_pop($tree);
 
-    return $value;
+    return $prepared;
   }
 
   public function getFieldKeys() {
