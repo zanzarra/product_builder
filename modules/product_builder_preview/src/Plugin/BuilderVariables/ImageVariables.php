ec0e081b5f4e85d7a402dd16b41ff40901280e40
Alex Liannoy <aliannoy@gmail.com>
2019-01-13 17:27:29 +0200

    Issue #3018378. Added Field Group integration. Removed redundant code. Fixed a couple of minor issues.

--- modules/product_builder_preview/src/Plugin/BuilderVariables/ImageVariables.php
+++ modules/product_builder_preview/src/Plugin/BuilderVariables/ImageVariables.php
@@ -18,10 +18,6 @@ class ImageVariables extends BuilderVariablesPluginBase {
    * {@inheritdoc}
    */
   public function prepare($value) {
-    if(!isset($value['target_id'])) {
-      return '';
-    }
-
     if (!$value || !$image = \Drupal::entityTypeManager()->getStorage('file')->load($value['target_id'])) {
       return parent::prepare($value);
     }
